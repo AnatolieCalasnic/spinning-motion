920c6287ca5eda84892b2fc25c39b4bb42710ad5
Anatolii Calaşnic <522476@student.fontys.nl>
2024-09-24 09:44:11 +0200

    updating for pipeline

--- src/main/java/org/myexample/spinningmotion/SpinningMotionApplication.java
+++ src/main/java/org/myexample/spinningmotion/SpinningMotionApplication.java
@@ -2,7 +2,6 @@ package org.myexample.spinningmotion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpinningMotionApplication {
 
