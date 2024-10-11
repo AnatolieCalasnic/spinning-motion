55ab498b93c5e46d069e385175ce4bad32df9090
Anatolii Calaşnic <522476@student.fontys.nl>
2024-10-11 16:22:53 +0200

    Some controller logic change and added dockerfile to my project

--- src/main/java/org/myexample/spinningmotion/business/impl/GenreUseCaseImpl.java
+++ src/main/java/org/myexample/spinningmotion/business/impl/GenreUseCaseImpl.java
@@ -8,7 +8,9 @@ import org.myexample.spinningmotion.domain.genre.*;
 import org.myexample.spinningmotion.persistence.GenreRepository;
 import org.myexample.spinningmotion.persistence.entity.GenreEntity;
 import org.springframework.stereotype.Service;
+import jakarta.annotation.PostConstruct;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +19,12 @@ import java.util.stream.Collectors;
 public class GenreUseCaseImpl implements GenreUseCase {
     private final GenreRepository genreRepository;
 
+    @PostConstruct
+    public void initializeGenres() {
+        if (!genreRepository.existsAny()) {
+            Arrays.stream(GenreEnum.values()).forEach(this::createGenre);
+        }
+    }
     @Override
     public GetGenreResponse getGenre(GetGenreRequest request) {
         GenreEntity genre = genreRepository.findById(request.getId())
@@ -34,9 +42,11 @@ public class GenreUseCaseImpl implements GenreUseCase {
     }
     @Override
     public void createGenre(GenreEnum genreEnum) {
-        GenreEntity genreEntity = new GenreEntity();
-        genreEntity.setName(genreEnum.getDisplayName());
-        genreRepository.save(genreEntity);
+        if (!genreRepository.existsByName(genreEnum.getDisplayName())) {
+            GenreEntity genreEntity = new GenreEntity();
+            genreEntity.setName(genreEnum.getDisplayName());
+            genreRepository.save(genreEntity);
+        }
     }
     private GetGenreResponse convertToResponse(GenreEntity entity) {
         return GetGenreResponse.builder()
